8f3570b6bd28d1d63989620b4284e1f0e6061527
Sergey <yvarovsg@icloud.com>
2023-03-10 20:11:24 +0300

    Initial Commit

---
+++ TestAppShopAuth/TestAppShopAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppShopAuthApp.swift
+//  TestAppShopAuth
+//
+//  Created by AlkoFitoNiashka on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestAppShopAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
